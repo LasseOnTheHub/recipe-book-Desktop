96c04d211b25c8caaac44ad70df103f0573d3131
Lasse <lassebirkegaard91@gmail.com>
2017-03-05 23:48:06 +0100

    thfhtg

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import {Recipe} from "./recipe";
+import {Ingredient} from "../ingredient";
 
 @Injectable()
 export class RecipeService {
   private recipes: Recipe[]= [
-  new Recipe('Schnitzel','very tasty', 'http://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/matcha-mousse-cake.jpg',[]),
+  new Recipe('Schnitzel','very tasty', 'http://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/matcha-mousse-cake.jpg',[
+    new Ingredient('French Fries',2),
+    new Ingredient('Pork',1)
+  ]),
   new Recipe('Super Schnitzel','very mega tasty', 'http://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/rosewater-raspberry-sponge-cake.jpg',[])
 ];
   constructor() { }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Ingredient} from "../ingredient";
+import {ShoppingListService} from "./shopping-list.service";
 
 @Component({
   selector: 'rb-shopping-list',
@@ -9,9 +10,10 @@ export class ShoppingListComponent implements OnInit {
   ingredientsalt: Ingredient = new Ingredient("salt",2);
   items: Ingredient[] = [];
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
+    this.items = this.sls.getItems();
   }
 
 }
